a91c209392e64edf126fd5fcc5a9a5d95bc2e651
hanifali <ali.hanif@outlook.com>
2026-07-26 00:15:28 +0100

    removed hard coded api url for login

--- auth/handleLogin.tsx
+++ auth/handleLogin.tsx
@@ -21,7 +21,7 @@ export async function handleLogin({ username, password }: LoginPayload) {
         },
     };*/
 
-    const res = await fetch("http://localhost:8080/api/auth/login", {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/auth/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password }),
