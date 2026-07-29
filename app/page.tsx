8ea99587b7a73d8f2515f7886d4b4db6c982f337
hanifali <ali.hanif@outlook.com>
2026-07-29 15:53:17 +0100

    fixed homepage layout to support mobile

--- app/page.tsx
+++ app/page.tsx
@@ -14,8 +14,8 @@ export default function Home() {
 
     const [fadeOutRecent, setFadeOutRecent] = useState(false);
   return (
-    <div className={`flex h-[200vh]  w-full bg-zinc-900 font-sans ${fadeOutRecent ? "fadeOutRecent" : ""}`}>
-      <main className="w-full">
+    <div className={`homePage flex  w-full bg-zinc-900 font-sans ${fadeOutRecent ? "fadeOutRecent" : ""}`}>
+      <main className="w-full ">
         <div className="bg-[#FFC700] items-center w-full h-fit ">
           <div className="homeBg"></div>
 
@@ -23,7 +23,7 @@ export default function Home() {
           <div className="title title2 gradient-text">and a artist</div>
           <div className="title title3 gradient-text">welcome </div>
           
-          <div className="homeIntro homeBg gradient"></div>
+          <div className="homeIntro homeBg gradient "></div>
 
           <div className="homeIntro fixed top-[00px] w-full h-[95vh]">
             <video
@@ -45,18 +45,16 @@ export default function Home() {
 
           </div>
 
-          <div className="pt-[100px] text-center top-0 text-[320px] h-[70vh] text-[#222]/0 font-bold">porfolio</div>
+          <div className="pt-[100px] text-center top-0 text-[32px] h-[70vh] text-[#222]/0 font-bold"></div>
           <HomeNav />
 
-          <div className="home px-[100px] bg-zinc-900 h-fit justify-center pb-[200px] pt-[150px] z-20 relative text-4xl ">
-
+          <div className="home px-[100px] bg-zinc-900 h-fit justify-center pb-[200px] pt-[150px] z-20 relative text-4xl overflow-x-hidden">
             <AboutMe />
             <Divider />
             <div className="customBigHeading">RECENT</div>
             <Recent 
               fadeOutRecent={fadeOutRecent}
               setFadeOutRecent={setFadeOutRecent}/>
-
           </div>
 
         </div>
