7ab68f239e831b3f24826e3fcff71808880e4b03
hanifali <ali.hanif@outlook.com>
2026-08-12 00:25:46 +0100

    changes made to make qual  page to support mobile

--- app/qualification/page.tsx
+++ app/qualification/page.tsx
@@ -1,7 +1,10 @@
+"use client"
 import Info from "../components/info/Info"
 import Nav from "../components/navigation/Nav"
 import Qualification from "../components/qualification/qualification"
+import useScrollReveal from "../hooks/useScrollReveal"
 export default function qualificationPage() {
+    useScrollReveal(".offscreenDown", "easeIn", false);
     return (
         <>
             <div className="qualContainer">
@@ -11,7 +14,7 @@ export default function qualificationPage() {
                     <Nav />
                     <Qualification />
                 </div>
-                <div className="topFadeHomePage"></div>
+                <div className="topFadeHomePage offscreenDown"></div>
             </div>
 
         </>
