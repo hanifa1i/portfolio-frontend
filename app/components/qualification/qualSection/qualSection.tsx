d0b1945cd90b4c45eb3746b87e7d54516813c525
hanifali <ali.hanif@outlook.com>
2026-08-11 17:28:19 +0100

    changes made to make qual  page to support mobile

--- app/components/qualification/qualSection/qualSection.tsx
+++ app/components/qualification/qualSection/qualSection.tsx
@@ -10,8 +10,9 @@ export default function qualSection({ qualNumber, qualSelected, data }: Props) {
         <>
             <div className={`${styles.section} ${qualNumber !== qualSelected ? styles.unselected : styles.selected}`}>
                 <div className={`${styles.qualInfoContainer} ${qualNumber !== qualSelected ? styles.unselectedInfo : ""}`}>
-                    <div className={styles.col1}>
                     <div className={` ${styles.qualDate}`}>{data.start_date.slice(0, 4)}-{data.end_date.slice(0, 4)}</div>
+
+                    <div className={styles.col1}>
                     <div className={` ${styles.qualLevel}`}>{data.level}</div>
                     <div className={` ${styles.qualSubject}`}>{data.qualification}</div>
                     <div className={` ${styles.qualInstitution}`}>at {data.institution}</div>
