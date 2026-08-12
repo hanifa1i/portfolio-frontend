c89bcc947a93d1d59b5d01b89f4fca99dccc72c7
hanifali <ali.hanif@outlook.com>
2026-08-12 16:34:25 +0100

    experience page support for mobile

--- app/components/common/Divider.tsx
+++ app/components/common/Divider.tsx
@@ -5,7 +5,7 @@ import { useEffect, useRef, useState } from "react";
 export default function Divider() {
     return (
         <>
-            <div className="border border-[#222] h-[1px] my-[70px] mx-auto max-w-[1300px] "/>
+            <div className="border border-[#222] h-[1px] my-[70px] mx-auto max-w-[1300px] max-[800px]:my-[40px] "/>
         </>
     );
 }
