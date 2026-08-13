d26e8cb236c08f80ddde25f23d5c6c882b953da6
hanifali <ali.hanif@outlook.com>
2026-08-13 14:46:20 +0100

    experience page support for mobile

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -1,16 +1,20 @@
 import Nav from "../components/navigation/Nav"
 import Experience from "../components/experience/experience"
 import Info from "../components/info/Info"
+import useScrollReveal from "../hooks/useScrollReveal";
 export default function experiencePage() {
+    useScrollReveal(".offscreenDown", "easeIn", false);
     return (
         <>
             <div className="expContainer">
 
                 <div className="pageCenterer">
-                    <Info/>
+                    <Info />
                     <Nav />
                     <Experience />
                 </div>
+                <div className="topFadeHomePage offscreenDown"></div>
+
             </div>
 
         </>
