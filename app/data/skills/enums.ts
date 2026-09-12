5ed5f7cee9f4de4cc3dbabeac1507d6c98c2292f
hanifali <ali.hanif@outlook.com>
2026-09-12 23:11:44 +0100

    updated some enums for skills

--- app/data/skills/enums.ts
+++ app/data/skills/enums.ts
@@ -3,8 +3,8 @@ export const skillTypeLabels = [
     "back-end & devOps",
     "testing",
     "databases & message brokers",
-    "API & design awareness",
-    "front-end & design awareness",
+    "API & integration",
+    "design",
     "security & best practices",
     "collaboration & workflow"
 ];
@@ -13,8 +13,8 @@ export const skillTypeList = [
     { enum: "BACKEND_AND_DEVOPS", label: "back-end & devOps" },
     { enum: "TESTING", label: "testing" },
     { enum: "DATABASES_AND_MESSAGE_BROKERS", label: "databases & message brokers" },
-    { enum: "API_AND_INTEGRATION", label: "API & design awareness" },
-    { enum: "FRONTEND_AND_DESIGN_AWARENESS", label: "front-end & design awareness" },
+    { enum: "API_AND_INTEGRATION", label: "API & integration" },
+    { enum: "DESIGN", label: "design" },
     { enum: "SECURITY_AND_BEST_PRACTICES", label: "security & best practices" },
     { enum: "COLLABORATION_AND_WORKFLOW", label: "collaboration & workflow" }
 ];
@@ -23,12 +23,14 @@ export const experiencedLabels = [
     "uxbridge college",
     "brunel university",
     "sparta global",
-    "land registry"];
+    "land registry",
+    "portfolio"];
 
 export const experienceList = [
     { enum: "SELF_STUDY", label: "self-study" },
     { enum: "UXBRIDGE_COLLAGE", label: "uxbridge college" },
     { enum: "BRUNEL_UNIVERSITY", label: "brunel university" },
     { enum: "SPARTA_GLOBAL", label: "sparta global" },
-    { enum: "HMLR", label: "land registry" }
+    { enum: "HMLR", label: "land registry" },
+    { enum: "PORTFOLIO", label: "portfolio" }
 ];
